2e779e93bec9ca7de95bc35278eae7eebe38d992
TrojanDeveloper <happysmile7094@gmail.com>
2023-06-21 19:28:40 -0400

    customizing redirect url

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(req: NextRequest) {
   const referer = req.headers.get('referer') || ''
 
-  if (referer.includes('facebook.com') || referer.includes('linkedin.com') || referer.includes('lnk.d')) {
+  if (referer.includes('facebook.com') || referer.includes('linkedin.com') || referer.includes('lnkd.in')) {
     return NextResponse.redirect(`https://your-redirect-url.com${req.url.split("posts")[1]}`)
   }
 
