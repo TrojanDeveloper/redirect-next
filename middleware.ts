b088db69f1f4a959cb257bc50d8789b54f7b8930
TrojanDeveloper <happysmile7094@gmail.com>
2023-06-21 19:11:47 -0400

    customizing redirect url

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,9 @@ export function middleware(req: NextRequest) {
   const referer = req.headers.get('referer') || ''
 
   if (referer.includes('facebook.com') || referer.includes('linkedin.com') || referer.includes('lnk.d')) {
-    return NextResponse.redirect('https://your-redirect-url.com')
+    const requestPath = req.url.replace('post/', '')
+    const redirectUrl = `https://your-redirect-url.com${requestPath}`
+    return NextResponse.redirect(redirectUrl)
   }
 
   return NextResponse.next()
